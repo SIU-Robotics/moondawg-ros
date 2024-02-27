2002de42d31528cb517e0c5ed2ab19ce37264d57
root <root@Andrew-PC>
2024-02-26 22:52:49 -0600

    moreeee

--- src/rpi_pkg/rpi_pkg/controller_node.py
+++ src/rpi_pkg/rpi_pkg/controller_node.py
@@ -1,7 +1,7 @@
 from std_msgs.msg import String
 import rclpy
 from rclpy.node import Node
-import keyboard
+from pynput.keyboard import Listener
 
 
 class ControlsPublisher(Node):
@@ -9,11 +9,16 @@ class ControlsPublisher(Node):
     def __init__(self):
         super().__init__('controller_node')
         self.publisher_ = self.create_publisher(String, 'control_commands', 10)
-        self.key_listener = keyboard.on_press(self.on_press)
-        self.get_logger().info('Starting')
+        # self.key_listener = Listener(on_press=self.on_press, on_release=self.on_release)
+        self.templisten()
+
+    def templisten(self):
+        while True:
+            cmd = input("Enter a command: ")
+            self.send_command(cmd)
+
 
     def on_press(self, key):
-        self.get_logger().info('Key pressed: %s' % key)
         try:
             if key.char == 'w':
                 self.send_command('forward')
@@ -27,7 +32,6 @@ class ControlsPublisher(Node):
             self.get_logger().error('Error parsing key press')
 
     def on_release(self, key):
-        self.get_logger().info('Key released: %s' % key)
         try:
             if key.char == 'w' or key.char == 's' or key.char == 'a' or key.char == 'd':
                 self.send_command('stop')
@@ -35,11 +39,10 @@ class ControlsPublisher(Node):
             self.get_logger().error('Error parsing key release')
 
     def send_command(self, command):
-        self.get_logger().info('Sending command: %s' % command)
         msg = String()
-        # msg.data = command
-        self.publisher_.publish(command)
-        self.get_logger().info('Sending' % msg.data)
+        msg.data = command
+        self.publisher_.publish(msg)
+        self.get_logger().info('Sending ' + msg.data)
 
 
 
